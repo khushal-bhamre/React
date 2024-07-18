e6878dfa6a71b6881979a770191063744f58eb94
Khushal bhamre <bhamarek0@gmail.com>
2024-07-18 18:09:34 +0530

    day2 react

---
+++ day2/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+
+
+const heading = React.createElement("h1",{id:"head",class:"heading"},"hello from react!");
+const root = ReactDOM.createRoot(document.getElementById("root"));        
+
+
+const parent = React.createElement("div",{id:"parent"},React.createElement("div",{id:"children"},[React.createElement("h1",{},"hello"),React.createElement("h1",{},"hello")]))
+root.render(parent);
